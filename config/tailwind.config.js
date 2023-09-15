0d8ff5bb4fddec7e0c09591135574e896c6ef807
Alejandro Piñón <jesuspiva1998@hotmail.com>
2023-09-14 22:05:41 -0600

    working on homepage and main

---
+++ config/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      backgroundColor: {
+        'color-01': '#62d5b4',
+        'color-02': '#184a4a',
+        
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
