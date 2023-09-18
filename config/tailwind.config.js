8c6dcc4b97a16c4fe4f28ca95648d4eb3b2d50bc
Alejandro Piñón <jesuspiva1998@hotmail.com>
2023-09-18 13:49:45 -0600

    updated styles and main page

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
       backgroundColor: {
         'color-01': '#62d5b4',
         'color-02': '#184a4a',
-        
+      },
+      boxShadow: {
+        'S01': '5px 5px rgba(0, 98, 90, 0.4), 10px 10px rgba(0, 98, 90, 0.3), 15px 15px rgba(0, 98, 90, 0.2), 20px 20px rgba(0, 98, 90, 0.1), 25px 25px rgba(0, 98, 90, 0.05)',
       },
     },
   },
